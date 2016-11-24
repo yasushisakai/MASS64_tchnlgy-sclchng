b25abbe4810e60c093e5dc87fec6564e47d426a9
Yasushi Sakai <ysshski@gmail.com>
2016-11-24 10:09:29 -0500

    refactoring

--- mindmap/ecosystem.mm
+++ mindmap/ecosystem.mm
@@ -1,91 +1,183 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1476789353496" ID="ID_678741101" MODIFIED="1476789383553" TEXT="Bikes">
-<node CREATED="1476789383978" ID="ID_1044694070" MODIFIED="1476789387236" POSITION="right" TEXT="Issue">
-<node CREATED="1476790021962" ID="ID_1304962136" MODIFIED="1476790032540" TEXT="Improving bike lane secrurity"/>
-</node>
-<node CREATED="1476789387648" ID="ID_147507331" MODIFIED="1476789390228" POSITION="right" TEXT="People">
-<node CREATED="1476789920162" ID="ID_1023858552" MODIFIED="1476789922420" TEXT="drivers">
-<node CREATED="1476789923426" ID="ID_1420195698" MODIFIED="1476789926156" TEXT="bikers">
-<node CREATED="1476789982042" ID="ID_356612209" MODIFIED="1476789992836" TEXT="how do they feel about drivers,">
-<node CREATED="1476789994689" ID="ID_1462876062" MODIFIED="1476790013773" TEXT="could it be improved by infrastructure?">
-<node CREATED="1476791156186" ID="ID_136484570" MODIFIED="1476791170963" TEXT="do you feel unsafe ">
-<node CREATED="1476791170965" ID="ID_435687517" MODIFIED="1476791175285" TEXT="from the road itself"/>
-<node CREATED="1476791175746" ID="ID_1802073179" MODIFIED="1476791183734" TEXT="a car driver"/>
+<node CREATED="1476789353496" ID="ID_678741101" MODIFIED="1476886026693" TEXT="Bikes">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789383978" ID="ID_1044694070" MODIFIED="1476886026693" POSITION="right" TEXT="Issue">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790021962" ID="ID_1304962136" MODIFIED="1476886026693" TEXT="Improving bike lane secrurity">
+<font NAME="Roboto" SIZE="24"/>
 </node>
 </node>
-<node CREATED="1476790014713" ID="ID_751473639" MODIFIED="1476790018596" TEXT="or by norm?"/>
+<node CREATED="1476789387648" ID="ID_147507331" MODIFIED="1476886026693" POSITION="right" TEXT="People">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789920162" ID="ID_1023858552" MODIFIED="1476886026693" TEXT="drivers">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789923426" ID="ID_1420195698" MODIFIED="1476886026693" TEXT="bikers">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789982042" ID="ID_356612209" MODIFIED="1476886026693" TEXT="how do they feel about drivers,">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789994689" ID="ID_1462876062" MODIFIED="1476886026693" TEXT="could it be improved by infrastructure?">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476791156186" ID="ID_136484570" MODIFIED="1476886026693" TEXT="do you feel unsafe ">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476791170965" ID="ID_435687517" MODIFIED="1476886026693" TEXT="from the road itself">
+<font NAME="Roboto" SIZE="24"/>
 </node>
-</node>
-<node CREATED="1476789926481" ID="ID_785791714" MODIFIED="1476789935189" TEXT="car drivers">
-<node CREATED="1476789937625" ID="ID_883490878" MODIFIED="1476789980021" TEXT="how do they feel about bikers">
-<node CREATED="1476791044499" ID="ID_323237270" MODIFIED="1476791092645" TEXT="how much do we need to persuade to take down a parking lot to add bike lanes"/>
-<node CREATED="1476791093378" ID="ID_1352436039" MODIFIED="1476791143149" TEXT="do they think it will increase accidents if roads were narrower"/>
-</node>
-</node>
-<node CREATED="1476789943369" ID="ID_1117295553" MODIFIED="1476789946804" TEXT="city hall">
-<node CREATED="1476789947913" ID="ID_1396763295" MODIFIED="1476790142740" TEXT="how do they prioritize which street to improve first?"/>
-<node CREATED="1476790151481" ID="ID_915757747" MODIFIED="1476790170917" TEXT="is the efficiency of bike commute known?"/>
-</node>
-<node CREATED="1476790038193" ID="ID_821404729" MODIFIED="1476790121075" TEXT="pedestrians">
-<node CREATED="1476790044210" ID="ID_254026243" MODIFIED="1476790056341" TEXT="how do they feel about bikers"/>
+<node CREATED="1476791175746" ID="ID_1802073179" MODIFIED="1476886026693" TEXT="a car driver">
+<font NAME="Roboto" SIZE="24"/>
 </node>
 </node>
 </node>
-<node CREATED="1476790186001" ID="ID_1066249010" MODIFIED="1476790189333" POSITION="right" TEXT="Tools">
-<node CREATED="1476790191242" ID="ID_224918802" MODIFIED="1476790201725" TEXT="Physical intervention">
-<node CREATED="1476790202874" ID="ID_1251149625" MODIFIED="1476790206765" TEXT="Painting roads"/>
-</node>
-<node CREATED="1476790208209" ID="ID_523627059" MODIFIED="1476790230853" TEXT="collective data visualisation">
-<node CREATED="1476790237554" ID="ID_859974293" MODIFIED="1476790245629" TEXT="safe roads and dangerous roads"/>
+<node CREATED="1476790014713" ID="ID_751473639" MODIFIED="1476886026693" TEXT="or by norm?">
+<font NAME="Roboto" SIZE="24"/>
 </node>
 </node>
-<node CREATED="1476789390920" ID="ID_869220762" MODIFIED="1476790306546" POSITION="right" TEXT="Am I the One?">
-<node CREATED="1476790309498" ID="ID_1848929674" MODIFIED="1476790338398" TEXT="I think I am">
-<node CREATED="1476789407128" ID="ID_829830166" MODIFIED="1476789533607" TEXT="developing web applications for civic participation">
-<node CREATED="1476789460641" ID="ID_1323986914" MODIFIED="1476789474516" TEXT="architecture version of github"/>
-<node CREATED="1476789475088" ID="ID_368387121" MODIFIED="1476789520204" TEXT="shortest path + evacuation simulation"/>
 </node>
-<node CREATED="1476789534729" ID="ID_626473815" MODIFIED="1476789570036" TEXT="used different modes of transporation">
-<node CREATED="1476789572096" ID="ID_1836334160" MODIFIED="1476789592788" TEXT="8 years of bikes"/>
-<node CREATED="1476789593281" ID="ID_424829379" MODIFIED="1476789613148" TEXT="6 years of cars"/>
-<node CREATED="1476789637785" ID="ID_599156477" MODIFIED="1476789648436" TEXT="10 years of train"/>
+<node CREATED="1476789926481" ID="ID_785791714" MODIFIED="1476886026692" TEXT="car drivers">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789937625" ID="ID_883490878" MODIFIED="1476886026692" TEXT="how do they feel about bikers">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476791044499" ID="ID_323237270" MODIFIED="1476886026688" TEXT="how much do we need to persuade to take down a parking lot to add bike lanes">
+<font NAME="Roboto" SIZE="24"/>
 </node>
-<node CREATED="1476790364257" ID="ID_93287760" MODIFIED="1476790387981" TEXT="have done a 400mile trip on bikes"/>
-<node CREATED="1476789681369" ID="ID_248137966" MODIFIED="1476790325200" TEXT="have been facing people affected by disaster relief by voluntary projects">
-<node CREATED="1476789772665" ID="ID_1119352078" MODIFIED="1476789775684" TEXT="China"/>
-<node CREATED="1476789776281" ID="ID_1305211739" MODIFIED="1476789778924" TEXT="Haiti"/>
-<node CREATED="1476789779352" ID="ID_1098328918" MODIFIED="1476789783372" TEXT="Japan">
-<node CREATED="1476789791561" ID="ID_553885566" MODIFIED="1476789798332" TEXT="Tohoku"/>
-<node CREATED="1476789799432" ID="ID_1090607710" MODIFIED="1476789801932" TEXT="Kyushu"/>
-</node>
-<node CREATED="1476789783969" ID="ID_1939978340" MODIFIED="1476789790076" TEXT="(Washington)"/>
-</node>
-<node CREATED="1476789650121" ID="ID_1756740612" MODIFIED="1476789689923" TEXT="studying architecture and urban planning">
-<node CREATED="1476789835449" ID="ID_97552597" MODIFIED="1476789909221" TEXT="but rather interested in inviting normal people, then having professional people"/>
+<node CREATED="1476791093378" ID="ID_1352436039" MODIFIED="1476886026685" TEXT="do they think it will increase accidents if roads were narrower">
+<font NAME="Roboto" SIZE="24"/>
 </node>
 </node>
-<node CREATED="1476790339737" ID="ID_1754216892" MODIFIED="1476790346941" TEXT="I don&apos;t think I am">
-<node CREATED="1476790347930" ID="ID_599427150" MODIFIED="1476790359061" TEXT="have not lived here for enough">
-<node CREATED="1476790394994" ID="ID_109007064" MODIFIED="1476790406477" TEXT="climate is different"/>
 </node>
-<node CREATED="1476790360034" ID="ID_1486201415" MODIFIED="1476790483421" TEXT="does not know how much the car industry is strong"/>
-<node CREATED="1476790656730" ID="ID_1504796795" MODIFIED="1476790673085" TEXT="boston city cyclists union"/>
+<node CREATED="1476789943369" ID="ID_1117295553" MODIFIED="1476886026685" TEXT="city hall">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789947913" ID="ID_1396763295" MODIFIED="1476886026685" TEXT="how do they prioritize which street to improve first?">
+<font NAME="Roboto" SIZE="24"/>
 </node>
-<node CREATED="1476790412906" ID="ID_1878428289" MODIFIED="1476790775764" TEXT="Is boston the best place?">
-<node CREATED="1476790420538" ID="ID_1507553098" MODIFIED="1476790423997" TEXT="I live here"/>
-<node CREATED="1476790424353" ID="ID_171856039" MODIFIED="1476790438050" TEXT="roads erode fast">
-<node CREATED="1476790438052" ID="ID_1446063611" MODIFIED="1476790442325" TEXT="climate"/>
+<node CREATED="1476790151481" ID="ID_915757747" MODIFIED="1476886026685" TEXT="is the efficiency of bike commute known?">
+<font NAME="Roboto" SIZE="24"/>
 </node>
 </node>
-<node CREATED="1476790777186" ID="ID_664513755" MODIFIED="1476790783405" TEXT="Is this the right time">
-<node CREATED="1476790784291" ID="ID_158993972" MODIFIED="1476790800093" TEXT="lots of similar techniques">
-<node CREATED="1476790801450" ID="ID_1547195758" MODIFIED="1476790805341" TEXT="people are aware"/>
-<node CREATED="1476790805634" ID="ID_1442019908" MODIFIED="1476790811509" TEXT="tools are present"/>
-</node>
-<node CREATED="1476790812818" ID="ID_301090229" MODIFIED="1476791033270" TEXT="https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/24713"/>
+<node CREATED="1476790038193" ID="ID_821404729" MODIFIED="1476886026685" TEXT="pedestrians">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790044210" ID="ID_254026243" MODIFIED="1476886026685" TEXT="how do they feel about bikers">
+<font NAME="Roboto" SIZE="24"/>
 </node>
 </node>
-<node CREATED="1476790299058" ID="ID_102414350" MODIFIED="1476790299058" POSITION="right" TEXT=""/>
+</node>
+</node>
+<node CREATED="1476790186001" ID="ID_1066249010" MODIFIED="1476886026685" POSITION="right" TEXT="Tools">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790191242" ID="ID_224918802" MODIFIED="1476886026685" TEXT="Physical intervention">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790202874" ID="ID_1251149625" MODIFIED="1476886026685" TEXT="Painting roads">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476790208209" ID="ID_523627059" MODIFIED="1476886026684" TEXT="collective data visualisation">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790237554" ID="ID_859974293" MODIFIED="1476886026684" TEXT="safe roads and dangerous roads">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+</node>
+<node CREATED="1476789390920" ID="ID_869220762" MODIFIED="1476886026684" POSITION="right" TEXT="Am I the One?">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790309498" ID="ID_1848929674" MODIFIED="1476886026684" TEXT="I think I am">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789407128" ID="ID_829830166" MODIFIED="1476886026684" TEXT="developing web applications for civic participation">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789460641" ID="ID_1323986914" MODIFIED="1476886026684" TEXT="architecture version of github">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789475088" ID="ID_368387121" MODIFIED="1476886026684" TEXT="shortest path + evacuation simulation">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476789534729" ID="ID_626473815" MODIFIED="1476886026684" TEXT="used different modes of transporation">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789572096" ID="ID_1836334160" MODIFIED="1476886026684" TEXT="8 years of bikes">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789593281" ID="ID_424829379" MODIFIED="1476886026684" TEXT="6 years of cars">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789637785" ID="ID_599156477" MODIFIED="1476886026684" TEXT="10 years of train">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476790364257" ID="ID_93287760" MODIFIED="1476886026683" TEXT="have done a 400mile trip on bikes">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789681369" ID="ID_248137966" MODIFIED="1476886026678" TEXT="have been facing people affected by disaster relief by voluntary projects">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789772665" ID="ID_1119352078" MODIFIED="1476886026678" TEXT="China">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789776281" ID="ID_1305211739" MODIFIED="1476886026678" TEXT="Haiti">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789779352" ID="ID_1098328918" MODIFIED="1476886026678" TEXT="Japan">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789791561" ID="ID_553885566" MODIFIED="1476886026678" TEXT="Tohoku">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476789799432" ID="ID_1090607710" MODIFIED="1476886026678" TEXT="Kyushu">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476789783969" ID="ID_1939978340" MODIFIED="1476886026678" TEXT="(Washington)">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476789650121" ID="ID_1756740612" MODIFIED="1476886026678" TEXT="studying architecture and urban planning">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476789835449" ID="ID_97552597" MODIFIED="1476886026672" TEXT="but rather interested in inviting normal people, then having professional people">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+</node>
+<node CREATED="1476790339737" ID="ID_1754216892" MODIFIED="1476886026672" TEXT="I don&apos;t think I am">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790347930" ID="ID_599427150" MODIFIED="1476886026672" TEXT="have not lived here for enough">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790394994" ID="ID_109007064" MODIFIED="1476886026672" TEXT="climate is different">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476790360034" ID="ID_1486201415" MODIFIED="1476886026672" TEXT="does not know how much the car industry is strong">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476790412906" ID="ID_1878428289" MODIFIED="1476886026672" TEXT="Is boston the best place?">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790420538" ID="ID_1507553098" MODIFIED="1476886026672" TEXT="I live here">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476790656730" HGAP="23" ID="ID_1504796795" MODIFIED="1476886026672" TEXT="boston city cyclists union" VSHIFT="7">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476790424353" ID="ID_171856039" MODIFIED="1476886026672" TEXT="roads erode fast">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790438052" ID="ID_1446063611" MODIFIED="1476886026672" TEXT="climate">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476792009239" ID="ID_166452089" MODIFIED="1476886026672" TEXT="walkable place compared to other cities in U.S.">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476790777186" ID="ID_664513755" MODIFIED="1476886026672" TEXT="Is this the right time">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790784291" ID="ID_158993972" MODIFIED="1476886026671" TEXT="lots of similar techniques">
+<font NAME="Roboto" SIZE="24"/>
+<node CREATED="1476790801450" ID="ID_1547195758" MODIFIED="1476886026671" TEXT="people are aware">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+<node CREATED="1476790805634" ID="ID_1442019908" MODIFIED="1476886026671" TEXT="tools are present">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1476790812818" ID="ID_301090229" MODIFIED="1476886026663" TEXT="https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/24713">
+<font NAME="Roboto" SIZE="24"/>
+</node>
+</node>
+</node>
 </node>
 </map>
